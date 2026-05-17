e3101a98ab2997eb3494a41bf8394c83bd9d4c99
fehzitu <fehzitujsdev@gmail.com>
2026-05-17 14:09:50 -0300

    add some details to profile

--- src/components/embeds/profilePage.js
+++ src/components/embeds/profilePage.js
@@ -12,6 +12,10 @@ module.exports = function createProfilePage(client, user) {
             .addFields([{
                 name: `👤 **${user.globalName}**`,
                 value: `>>> ⏳ **Lv.${profile.rpg.level}『 ${profile.rpg.xp}xp / ${Math.floor(100 * Math.pow(profile.rpg.level + 1, 1.5))}xp 』**💸 **R$${profile.rpg.money}**\n🧿 **Personalidade: ${profile.karma.personality}**\n📚 **Mensagens: ${profile.stats.messages}**\n📡 **Comandos: ${profile.stats.commands}**`
+            },
+            {
+                name: '🎖️ **Honra**',
+                value: `>>> 🏆 **Conquistas: 『${profile.honor.achievements.length} / 50』**\n🏅 **Medalhas: 『${profile.honor.medals.length} / 25』**\n✨ **Prestigio: 『${profile.honor.prestige.length}』**`
             }])
             .setImage('https://i.pinimg.com/originals/fb/7d/25/fb7d25365c6f2deca04b86f35c8fee63.gif')
             .setTimestamp()
