864260c8caba516a0d75ff4d2bb24d5d27be46f0
fehzitu <fehzitujsdev@gmail.com>
2026-05-17 09:08:45 -0300

    fix some txt

--- src/components/embeds/supportPage.js
+++ src/components/embeds/supportPage.js
@@ -6,8 +6,8 @@ module.exports = function createSupportPage(user) {
     return (
         createEmbed(user)
             .addFields([{
-                name: '**🤖 Ainda estou em desenvolvimento então pode ser que algo de errado ou inesperado aconteça.\nSe for o caso informe:**\n<@830634043560296468> | <@1335722284576407562>',
-                value: '**☕ Bora tomar um café?\n__https://discord.gg/Wpgu4qXWUk__**'
+                name: '**🤖 Ainda estou em desenvolvimento então pode ser que algo de errado ou inesperado aconteça.Se for o caso informe aos desenvolvedores ou o suporte no nosso servidor:\n__https://discord.gg/Wpgu4qXWUk__**',
+                value: '**☕ Bora tomar um café?**'
             }])
             .setImage('https://cdn.discordapp.com/attachments/1477290272638632068/1491798480216264824/93f04b4b1103cc4e6410bb4f831acb6c.gif?ex=69d90119&is=69d7af99&hm=ca14d33797d593d0914af41ff129a00fc1f87c8ab1ce3a564058456fc749bb34&')
             .setTimestamp()
