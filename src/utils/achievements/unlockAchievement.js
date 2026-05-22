f9a424ef6c38d6818b90af39711c82dcfe6fb292
fehzitu <fehzitujsdev@gmail.com>
2026-05-22 09:22:18 -0300

    first achievements checker

---
+++ src/utils/achievements/unlockAchievement.js
@@ -0,0 +1,28 @@
+// import achievements list
+const achievementsList = require('../../structures/achievementsList');
+
+// unlock an achievement
+function unlockAchievement(profile, achievementId) {
+    // find achievement
+    const achievement = achievementsList.find(achievement => achievement.id === achievementId);
+
+    // achievement not found
+    if (!achievement) return null;
+
+    // already unlocked
+    if (profile.honor.achievements.includes(
+            achievementId)) return null;
+
+    // unlock achievement
+    profile.honor.achievements.push(achievementId);
+
+    // give rewards
+    if (achievement.reward?.xp) profile.rpg.xp += achievement.reward.xp;
+
+    if (achievement.reward?.money) profile.rpg.money += achievement.reward.money;
+
+    // return unlocked achievement
+    return achievement;
+};
+
+module.exports = unlockAchievement;
