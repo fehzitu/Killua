45976a8bed593c11ee1f7314cd2c7d6198de92ad
fehzitu <fehzitujsdev@gmail.com>
2026-05-26 23:38:06 -0300

    details

--- src/config/index.js
+++ src/config/index.js
@@ -3,22 +3,22 @@ require('dotenv').config();
 module.exports = {
     // bot core
     token: process.env.TOKEN,
-    prefix: process.env.BOT_PREFIX || "!",
+    prefix: process.env.BOT_PREFIX || '!',
 
     // environment
-    isDev: process.env.NODE_ENV === "development",
+    isDev: process.env.NODE_ENV === 'development',
 
     // database
     database: {
-        usersPath: process.env.USERS_PATH || "./users.json"
+        usersPath: process.env.USERS_PATH || './users.json'
     },
 
     // discord settings
     discord: {
         intents: [
-            "GUILDS",
-            "GUILD_MESSAGES",
-            "MESSAGE_CONTENT"
+            'GUILDS',
+            'GUILD_MESSAGES',
+            'MESSAGE_CONTENT'
         ]
     }
 };
