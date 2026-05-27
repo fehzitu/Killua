8e2b02307a9de06006b914e0d0589ea645a69d6f
fehzitu <fehzitujsdev@gmail.com>
2026-05-26 23:58:42 -0300

    adding a checker on achievements embed

--- src/components/embeds/achievementsPage.js
+++ src/components/embeds/achievementsPage.js
@@ -1,19 +1,26 @@
 // import custom functions
 const createEmbed = require('../../utils/embed');
+const ensureProfile = require('../../utils/ensureProfile');
 
 // import achievements list
 const achievementsList = require('../../structures/achievementsList');
 
 // return an embed with the page
 module.exports = function createAchievementsPage(user, achievementIndex) {
+    // get the user profile
+    const profile = ensureProfile(client, user);
+
     // get the achievement
     const achievement = achievementsList[achievementIndex];
+    
+    // check if user have this achievement
+    const obtained = '...'; // finish later
 
     return (
         createEmbed(user)
             .addFields([{
                 name: `🏆 **Detalhes da conquista:**\n>>> 📝 ${achievement.name}\n${achievement.goal}`,
-                value: `🤑 Recompensas:\n>>> ✨ +${achievement.reward?.xp || 0} Xp\n💸 +R$ ${achievement.reward?.money || 0}`
+                value: `🤑 **Recompensas:**\n> ✨ +${achievement.reward?.xp || 0} Xp\n> 💸 +R$ ${achievement.reward?.money || 0}\n🫡 **Obtida?**\n${obtained}`
             }])
     );
 };
