567b2cf956864b48152881e2c0474d9e56b140bf
fehzitu <fehzitujsdev@gmail.com>
2026-06-03 22:50:34 -0300

    fix some details

--- src/components/embeds/ranking/messageRank.js
+++ src/components/embeds/ranking/messageRank.js
@@ -5,17 +5,17 @@ const sortByCategory = require('../../../utils/sortByCategory.js');
 // return an embed with the page
 module.exports = function createLevelRankPage(client, user) {
     // get the top 5
-    const ranking = sortByCategory(client.usersData, 'rpg.level').slice(0, 5);
+    const ranking = sortByCategory(client.usersData, 'stats.messages').slice(0, 5);
 
     // clean list
-    const list = ranking.map(user => `👤 **<@${user.id}>** | **↗️ ${user.stats.messages}**`).join('\n') || 'Nenhum usuário encontrado.';
+    const list = ranking.map(user => `👤 **<@${user.id}>** | **💬 ${user.stats.messages}**`).join('\n') || 'Nenhum usuário encontrado.';
 
     return (
         createEmbed(user)
             .addFields([{
                 name: '**💬 Top mensagens**',
-                value: `${list}\n\n✨️ **Parabéns aos ${ranking.length} mais upados!**`
+                value: `${list}\n\n✨️ **Parabéns aos ${ranking.length} mais falantes!**`
             }])
-            .setImage('https://cdn.discordapp.com/attachments/1477290272638632068/1496180164088958976/Animated_bar_chart.gif?ex=69e8f1dc&is=69e7a05c&hm=0ef944418ed41d4f37b90c3345a1da453a87ca4e1f7dccf1421cd37270f68493&')
+            .setImage('https://cdn.discordapp.com/attachments/1477290272638632068/1496256443790266398/Animacoes_Legais.gif?ex=69e938e7&is=69e7e767&hm=5587a339c9ec632888b5f0e14541e3bbe245740da38283b6973e26809e13a9d3&')
     );
 };
