e86df26336ad831234762c4ec2ae58a31f812ba3
fehzitu <fehzitujsdev@gmail.com>
2026-06-17 12:19:15 -0300

    embed v1

--- src/components/embeds/menu/achievementPage.js
+++ src/components/embeds/menu/achievementPage.js
@@ -1,13 +1,22 @@
 // import custom functions
 const createEmbed = require('../../../utils/components/embed');
 
+// import achievements list
+const achievementsList = require('../../../structures/achievementsList');
+
 // return an embed with the page
 module.exports = function createAchievementPage(user) {
+    // get achievements list
+    let list = '';
+
+    // clean list map
+    achievementsList.map((a) => list += `**${a.icon} ${a.name} (${a.rarity})\n🎯 ${a.goal}\n✨️ Recompensa: Xp: ${a.reward.xp} Dinheiro: R$${a.reward.money}**\n\n`);
+
     return (
         createEmbed(user)
             .addFields([{
                 name: '🏅 **Lista completa das conquistas**',
-                value: 'list[0].names'
+                value: list
             }])
     );
 };
