37a73b8297b85b2c7495b55c31a54b33e1af2a44
fehzitu <fehzitujsdev@gmail.com>
2026-07-10 00:48:39 -0300

    selector working

---
+++ src/interactions/selectors/interactionPageSelector.js
@@ -0,0 +1,30 @@
+// select
+module.exports = {
+    customId: 'interactionPageSelector',
+    async execute(interaction) {
+        const [id, ownerId] = interaction.customId.split(':');
+
+        // user restriction
+        if (ownerId && interaction.user.id !== ownerId) {
+            return interaction.reply({
+                content: '❌ Isso não é pra você!',
+                ephemeral: true
+            });
+        };
+
+        const value = interaction.values?.[0];
+
+        // safety check
+        if (!value) {
+            return interaction.reply({
+                content: '⚠️ Nenhuma opção selecionada.',
+                ephemeral: true
+            });
+        };
+
+        return interaction.reply({
+            content: `⚜️ Você escolheu: <@${value}>`,
+            ephemeral: true
+        });
+    }
+};
