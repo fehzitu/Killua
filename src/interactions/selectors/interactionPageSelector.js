2823a213e49bc51c19461fb060ac3773a5e8dc3b
fehzitu <fehzitujsdev@gmail.com>
2026-07-10 13:18:10 -0300

    add some buttons

--- src/interactions/selectors/interactionPageSelector.js
+++ src/interactions/selectors/interactionPageSelector.js
@@ -5,6 +5,21 @@ const { MessageActionRow } = require('discord.js');
 const createMenuButton = require('../../components/buttons/menuPage');
 const createInteractionSelectorPage = require('../../components/embeds/interaction/interactionSelectorPage');
 
+// import custom positive interactions buttons
+const createKissButton = require('../../components/buttons/interaction/kiss');
+const createHugButton = require('../../components/buttons/interaction/hug');
+const createGreetButton = require('../../components/buttons/interaction/greet');
+const createLaughtButton = require('../../components/buttons/interaction/laught');
+const createPartyButton = require('../../components/buttons/interaction/party');
+
+// import custom negative interactions buttons
+const createPunchButton = require('../../components/buttons/interaction/punch');
+const createKickButton = require('../../components/buttons/interaction/kick');
+const createSlapButton = require('../../components/buttons/interaction/slap');
+const createProvokeButton = require('../../components/buttons/interaction/provoke');
+const createKillButton = require('../../components/buttons/interaction/kill');
+
+
 // select
 module.exports = {
     customId: 'interactionPageSelector',
@@ -40,9 +55,24 @@ module.exports = {
         // menu button
         const menuButton = createMenuButton(user);
 
+// positive interaction buttons
+        const kissButton = createKissButton(user).setStyle('SUCCESS').setDisabled(true);
+        const hugButton = createHugButton(user).setStyle('SUCCESS').setDisabled(true);
+        const greetButton = createGreetButton(user).setStyle('SUCCESS').setDisabled(true);
+        const laughtButton = createLaughtButton(user).setStyle('SUCCESS').setDisabled(true);
+        const partyButton = createPartyButton(user).setStyle('SUCCESS').setDisabled(true);
+
+        // negative interaction buttons
+        const punchButton = createPunchButton(user).setStyle('DANGER').setDisabled(true);
+        const kickButton = createKickButton(user).setStyle('DANGER').setDisabled(true);
+        const slapButton = createSlapButton(user).setStyle('DANGER').setDisabled(true);
+        const provokeButton = createProvokeButton(user).setStyle('DANGER').setDisabled(true);
+        const killButton = createKillButton(user).setStyle('DANGER').setDisabled(true);
+
         // rows
-        const buttonRow = new MessageActionRow().addComponents(); // add components
-        const buttonRow2 = new MessageActionRow().addComponents(menuButton);
+        const buttonRow = new MessageActionRow().addComponents(kissButton, hugButton, greetButton, laughtButton, partyButton);
+        const buttonRow2 = new MessageActionRow().addComponents(punchButton, kickButton, slapButton, provokeButton, killButton);
+        const buttonRow3 = new MessageActionRow().addComponents(menuButton);
 
         // create an interaction selector page
         const interactionSelector = createInteractionSelectorPage(user, target.user);
@@ -50,7 +80,7 @@ module.exports = {
         // reset embed
         return interaction.update({
             embeds: [interactionSelector],
-            components: [buttonRow2]
+            components: [buttonRow, buttonRow2, buttonRow3]
         });
     }
 };
