2ed7bda372f17b791fab9adcba27e87dc6a954ff
fehzitu <fehzitujsdev@gmail.com>
2026-07-10 01:10:44 -0300

    logic

--- src/interactions/selectors/interactionPageSelector.js
+++ src/interactions/selectors/interactionPageSelector.js
@@ -7,6 +7,11 @@ module.exports = {
     async execute(interaction) {
         const [id, ownerId] = interaction.customId.split(':');
 
+        // get client, user and guild
+        const user = interaction.user;
+        const client = interaction.client;
+        const guild = interaction.guild;
+
         // user restriction
         if (ownerId && interaction.user.id !== ownerId) {
             return interaction.reply({
@@ -17,6 +22,9 @@ module.exports = {
 
         const value = interaction.values?.[0];
 
+        // get target
+        const target = await guild.members.fetch(`${value}`);
+
         // safety check
         if (!value) {
             return interaction.reply({
@@ -26,7 +34,7 @@ module.exports = {
         };
 
         // create an interaction selector page
-        const interactionSelector = createInteractionSelectorPage();
+        const interactionSelector = createInteractionSelectorPage(user, target);
 
         // reset embed
         return interaction.update({
