a15e0948c6e52301871c87c1a4baead5a62acde6
fehzitu <fehzitujsdev@gmail.com>
2026-07-23 13:01:38 -0300

    roulette game

--- src/components/embeds/menu/gamePage.js
+++ src/components/embeds/menu/gamePage.js
@@ -7,7 +7,7 @@ module.exports = function createInteractionPage(user) {
         createEmbed(user)
             .addFields([{
                 name: '**🎮 Menu de jogos**',
-                value: '**😝 Ta afim de fazer algumas apostas e lucrar um pouco? E que tal jogar contra outras pessoas?\n\n🎮 Cada valor arrecadado aqui é apenas virtual mas poderá ser trocado por dinheiro real no futuro!**'
+                value: '**💸: Roleta - ``Roleta do casino``\n\n😝 Ta afim de fazer algumas apostas e lucrar um pouco? Cada valor arrecadado aqui é apenas virtual mas poderá ser trocado por dinheiro real no futuro!**'
             }])
             .setImage('https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWJpN3hqM3kxaG5pZHRvZmNnd2psMjJvdXpoanQwOTJ2bHl2cHNidSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZEkQofMp9jWhvPHj8C/giphy.gif')
     );
